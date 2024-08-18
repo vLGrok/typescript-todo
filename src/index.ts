a3ab3bbf5b51f6da2a0ed78e64c3e4dcfbe25032
Robert Kerr <code@scanhelper.com>
2024-08-18 17:20:16 -0400

    add inquirer prompting lib

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"),
@@ -10,8 +11,36 @@ let todos: TodoItem[] = [
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
 
+function displayTodoList(): void {
+    console.log(
+        `$(collection.userName)'s Todo list `
+        + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+
+enum Commands {
+    Quit = "Quit"
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+
 console.clear();
 console.log(`${collection.userName}'s Todo List `
-    + `(${ collection.getItemCounts().incomplete } items to do)`);
+    + `(${collection.getItemCounts().incomplete} items to do)`);
 
 collection.getTodoItems(true).forEach(item => item.printDetails());
+
+promptUser();
